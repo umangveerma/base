bff21458e2c0057773fbcb234fb52f187900d929
Kira272921 <sunset272921@gmail.com>
2022-06-27 09:27:34 +0530

    🧹 chore: clean up unnecessary files

--- components/minter.tsx
+++ components/minter.tsx
@@ -1,93 +1,24 @@
-import { createQR, encodeURL, TransactionRequestURLFields, findReference, validateTransfer, FindReferenceError, ValidateTransferError } from "@solana/pay";
-import {  Connection, Keypair,PublicKey } from "@solana/web3.js";
-import { useRouter } from "next/router";
-import { useEffect, useMemo, useRef } from "react";
-import toast, { Toaster } from 'react-hot-toast';
-import * as anchor from "@project-serum/anchor"
+import { createQR } from '@solana/pay';
+import { useEffect, useRef } from 'react';
 
 export default function Minter() {
+  const qrRef = useRef<HTMLDivElement>(null);
 
-    const router = useRouter()
-    // ref to a div where we'll show the QR code
-    const qrRef = useRef<HTMLDivElement>(null)
-    /* Add code to the component to get the Solana connection */
-  // Unique address that we can listen for payments to
-  const reference = useMemo(() => Keypair.generate().publicKey, [])
-  // Read the URL query (which includes our chosen products)
-  const searchParams = new URLSearchParams({ reference: reference.toString() });
-  for (const [key, value] of Object.entries(router.query)) {
-    if (value) {
-      if (Array.isArray(value)) {
-        for (const v of value) {
-          searchParams.append(key, v);
-        }
-      } else {
-        searchParams.append(key, value);
-      }
-    }
-  };
-
-  // Get a connection to Solana devnet
-const connection = new Connection(process.env.NEXT_PUBLIC_RPC!);
-  // Show the QR code
   useEffect(() => {
-    // window.location is only available in the browser, so create the URL in here
-    console.log(searchParams.toString())
-    const { location } = window
-    const apiUrl = `${location.protocol}//${location.host}/api/mintTransaction?${searchParams.toString()}`
-    console.log(apiUrl)
-    const urlParams: TransactionRequestURLFields = {
-      link: new URL(apiUrl),
-      label: "Candy Machine",
-      message: "https://www.downloadclipart.net/large/candy-png-free-download.png",
-    }
-    const solanaUrl = encodeURL(urlParams)
-    const qr = createQR(solanaUrl, 512, 'transparent')
+    const qr = createQR(
+      'solana:https%3A%2F%2Fapi.candypay.fun%2Fapi%2Fv1%2Fmint%3Fid%3De57b2d2fd4882e8aa89af3e6d651b31f?label=Made+with+love+by+CandyPay&message=Thanks+for+minting+the+NFTs%21',
+      512,
+      'transparent'
+    );
     if (qrRef.current) {
-      qrRef.current.innerHTML = ''
-      qr.append(qrRef.current)
+      qrRef.current.innerHTML = '';
+      qr.append(qrRef.current);
     }
-  })
-
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      try {
-        // Check if there is any transaction for the reference
-        const signatureInfo = await findReference(connection, reference, { finality: 'confirmed' })
-        // Validate that the transaction has the expected recipient, amount and SPL token
-
-        // const validationResult = await validateTransfer(connection,signatureInfo.signature,{
-        //   recipient: new PublicKey('9kpML3MhVLPmASMDBYuaMzmFiCtdm3aityWu1pJZ1wR4'),
-        //   amount: anchor.BN(1*anchor.web3.LAMPORTS_PER_SOL),
-        //   reference:reference,
-        // })
-
-        // console.log("validation result",validationResult);
-
-        console.log('Success! signature here: ', signatureInfo.signature.toString());
-
-        toast.success('NFT mint successfully!');
-
-        router.push('/confirmed');
-      } catch (e) {
-        if (e instanceof FindReferenceError) {
-          // No transaction found yet, ignore this error
-          return;
-        }
-        toast.error('Minting failed!');
-        console.error('Unknown error', e)
-        clearInterval(interval)
-      }
-    }, 500)
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
+  });
 
   return (
     <>
-    <Toaster />
       <div ref={qrRef} />
     </>
-  )
+  );
 }
